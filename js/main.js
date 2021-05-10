4ab90c7facaee7792db155dfa5bd0abff8f437d1
arshad-qadri <arshadqadri321@gmail.com>
2021-05-10 13:03:31 +0530

    ch

--- js/main.js
+++ js/main.js
@@ -1,16 +1,34 @@
-// window.scrollBy({ 
-//   top: 100, // could be negative value
-//   left: 0, 
-//   behavior: 'smooth' 
-// });
+// --------constant & veriable section ------------------
+const mynavbar = document.getElementById("mynavbar");
+const navItem = document.querySelectorAll(".nav-item");
 
-// $(window).bind('scroll', function () {
-//     if ($(window).scrollTop() > 850) {
-//         $('#navbar').addClass('sticky');
-//         $('.arrow-down').addClass('sticky-fix');
-//     } else {
-//         $('#navbar').removeClass('sticky');
-//         $('.arrow-down').removeClass('sticky-fix');
-//     }
-// });
+// -------------preloading section----------------------
+var preloader = document.getElementById("loading");
+function myfunction() {
+  preloader.style.display = "none";
+}
+$(".navbar-collapse a").click(function () {
+  $(".navbar-collapse").collapse("hide");
+});
 
+// ---------------onscroll section-----------------------
+window.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop > 50) {
+    mynavbar.classList.add("fixed-top");
+  } else {
+    mynavbar.classList.remove("fixed-top");
+  }
+});
+
+// ---------------navlink onclick section-----------------------
+const mytest = () => {
+  if (navItem.classList.add("active")) {
+    navItem.classList.remove("active");
+  } else {
+    navItem.classList.add("active");
+  }
+};
+navItem.onclick = () => {
+  mytest();
+  console.log(navItem);
+};
--- php/contact.php
+++ php/contact.php
@@ -1,18 +1,47 @@
 <?php
-$name= $_REQUEST['name'];
-$email= $_REQUEST['email'];
-$message= $_REQUEST['message'];
+// $name= $_REQUEST['name'];
+// $email= $_REQUEST['email'];
+// $message= $_REQUEST['message'];
 
-if(empty($name) || empty($email) || empty($message))
+// if(empty($name) || empty($email) || empty($message))
+// {
+// 	echo "Please fill all the fields";
+// }
+// else
+// {
+// 	mail("arshadqadri321@gmail.com", "portfolio", $message,"From: $name<$email>");
+
+// echo "<script type='text/javascript'>alert('Message sent successfully.');
+// 	window.history.log(-1);
+// </script>";
+// }
+
+// =================================================================
+
+if(isset($_REQUEST['submit'])){
+
+
+if(($_REQUEST['name']=="") || ($_REQUEST['email']=="") || ($_REQUEST['message']=="" ))
 {
-	echo "Please fill all the fields";
+    
+    echo "<script type='text/javascript'>alert('Please fill all the fields.');</script>";
 }
 else
 {
-	mail("arshadqadri321@gmail.com", "portfolio", $message,"From: $name<$email>");
+$name= $_REQUEST['name'];
+$subject=$_REQUEST ['subject'];
+$email= $_REQUEST['email'];
+$message= $_REQUEST['message'];
+	$mailTo="arshadqadri321@gmail.com";
+	$subject="Portfolio website";
+	$txt="Name : ".$name.".\n\nMessage : ".$message.".\n\nEmail : ".$email;
+	$headers= "From-".$email;
+	mail($mailTo, $subject, $txt);
 
-echo "<script type='text/javascript'>alert('Message sent successfully.');
-	window.history.log(-1);
+echo "<script type='text/javascript'>alert('Message Sent Successfully.');
+window.location='../index.html';
 </script>";
-}
+}}
+
+
 ?>
