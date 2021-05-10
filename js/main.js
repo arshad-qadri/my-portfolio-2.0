2ea2ed6a01554cb6d541a720e9e4b28f198456ed
arshad-qadri <arshadqadri321@gmail.com>
2021-05-10 10:45:12 +0530

    new portfolio

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// window.scrollBy({ 
+//   top: 100, // could be negative value
+//   left: 0, 
+//   behavior: 'smooth' 
+// });
+
+// $(window).bind('scroll', function () {
+//     if ($(window).scrollTop() > 850) {
+//         $('#navbar').addClass('sticky');
+//         $('.arrow-down').addClass('sticky-fix');
+//     } else {
+//         $('#navbar').removeClass('sticky');
+//         $('.arrow-down').removeClass('sticky-fix');
+//     }
+// });
+
